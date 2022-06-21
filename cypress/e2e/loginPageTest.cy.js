28a7dcaf851193ca81922aba7345add66d7b8334
Shampad Sharkar <sompodsign@gmail.com>
2022-06-21 15:10:55 +0600

    deposit withdraw

--- cypress/e2e/loginPageTest.cy.js
+++ cypress/e2e/loginPageTest.cy.js
@@ -12,18 +12,18 @@ describe('User login tests', () => {
             loginPage.signUp();
         })
 
-    // it('UI Test 1.2 - User visit Home page and login with valid credentials', {
-    //     retries: {
-    //         runMode: 3,
-    //         openMode: 3
-    //     },
-    // },() => {
-    //     loginPage.getLoginPage()
-    //     loginPage.doLogin();
-    // })
-    //
-    // it('UI Test 1.3 - Logout user', {
-    // },() => {
-    //     loginPage.logOut();
-    // })
+    it('UI Test 1.2 - User visit Home page and login with valid credentials', {
+        retries: {
+            runMode: 3,
+            openMode: 3
+        },
+    },() => {
+        loginPage.getLoginPage()
+        loginPage.doLogin();
+    })
+
+    it('UI Test 1.3 - Logout user', {
+    },() => {
+        loginPage.logOut();
+    })
 })
